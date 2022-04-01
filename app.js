fb7fff26a9769a6538ea8ebffd11a206db396908
Anhnkd <anhnkd@hblab.vn>
2022-04-01 18:00:10 +0700

    fix: connect-mongo-db-2

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
 const express = require('express')
+require('core-js/stable');
 const app = express()
 
-let people = [
-    {
-      name: "Hannah Rickard",
-      number: "06-51-99-56-83",
-      id: 1
-    },
-    {
-      name: "Soongj jonasd",
-      number: "10987654",
-      id: 2
-    },
-    {
-      name: "Courtney Martinez",
-      number: "3691215",
-      id: 3
-    }
-  ]
   const { MongoClient, ServerApiVersion } = require('mongodb');
-  const uri = "mongodb+srv://anhnkd:190497@cluster0.v105k.mongodb.net/dev01?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://anhnkd:anhnkd190497@cluster0.v105k.mongodb.net/dev01?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
   client.connect(err => {
-    const collection = client.db("test").collection("devices");
+    const collection = client.db("dev01").collection("movies");
     // perform actions on the collection object
     client.close();
   });
-
 
   app.get('/', (request, response) => {
       response.send('<h1>makeHard For learn node</h1>')
   })
 
-  app.get('/api/people', (request, response) => {
-      response.json(people)
+  app.get('/api/movies', (request, response) => {
+    response.json(client)
   })
 
   app.listen(process.env.PORT || 3000, function(){
