7fa5a2857f458bbc030f0fb221e17a9b2d52e854
Anhnkd <anhnkd@hblab.vn>
2022-04-01 17:09:43 +0700

    feat: connect-mongodb

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ let people = [
       id: 3
     }
   ]
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+  const uri = "mongodb+srv://anhnkd:190497@cluster0.v105k.mongodb.net/dev01?retryWrites=true&w=majority";
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+  });
+
 
   app.get('/', (request, response) => {
       response.send('<h1>makeHard For learn node</h1>')
@@ -30,4 +39,3 @@ let people = [
   app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
-  
