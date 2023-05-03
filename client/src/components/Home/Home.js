f7c130368b5c3d49d183bc883a611d36dc23ca4f
anhnkd <anh.nkd@citigo.com.vn>
2023-05-03 22:57:08 +0700

    paginate

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect} from 'react'
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
 import useStyles from './styles';
+import Pagination from '../Pagination';
 
 export const Home = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -24,6 +25,9 @@ export const Home = () => {
 
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper className={classes.pagination} elevation={6}>
+                            <Pagination/>
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
