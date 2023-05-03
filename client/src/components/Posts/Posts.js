7322874e9a302272b40395e179e3c406c300b1f0
anhnkd <anh.nkd@citigo.com.vn>
2023-05-03 23:57:04 +0700

    fix: fix color loading

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -10,7 +10,7 @@ const Posts = ({ setCurrentId }) => {
     const classes = useStyles();
 
     return (
-        !posts.length ? <CircularProgress /> : (
+        !posts.length ? <CircularProgress style={{'color': '#ef1e7a'}}/> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
                 {posts.map((post) => (
                     <Grid key={post._id} item xs={12} sm={6}>
